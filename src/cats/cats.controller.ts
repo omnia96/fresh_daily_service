e199344b6afe576d729bea9343a3a585d77845f2
CityHunter <dongjun1997@outlook.com>
2021-01-14 14:15:41 +0800

    自定义管道

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,22 +1,18 @@
 import {
   Body,
   Controller,
-  Delete,
   Get,
-  HttpException,
-  HttpStatus,
   Param,
   ParseIntPipe,
   Post,
-  Res,
   UseFilters,
 } from '@nestjs/common';
-import { Response } from 'express';
 import { Cat } from './interface/cat.interface';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { ForbiddenException } from '../core/exceptions/forbidden.exception';
 import { HttpExceptionFilter } from '../core/filters/httpException.filter';
+import { ValidationPipe } from '../core/pipes/validation.pipe';
 
 @Controller('cats')
 @UseFilters(new HttpExceptionFilter())
@@ -24,7 +20,7 @@ export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
-  async create(@Body() createCatDto: CreateCatDto) {
+  async create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
 
